6d23c7e3c7f7a0e822703526c5d88158e251650e
Mads Nielsen <madsn2000@gmail.com>
2024-04-26 21:23:38 +0200

    Deleted unwanted files

--- build-DragonQuest-Desktop-Debug/CMakeFiles/DragonQuest.dir/battle.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DragonQuest.dir/battle.cpp.o: \
- /home/mads/DragonQuest/DragonQuest/battle.cpp /usr/include/stdc-predef.h \
- /home/mads/DragonQuest/DragonQuest/battle.h
--- build-DragonQuest-Desktop-Debug/CMakeFiles/DragonQuest.dir/monster.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DragonQuest.dir/monster.cpp.o: \
- /home/mads/DragonQuest/DragonQuest/monster.cpp \
- /usr/include/stdc-predef.h /home/mads/DragonQuest/DragonQuest/monster.h
